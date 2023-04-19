fff47445dd473687ca041ec9864adc563291b77d
Ahetesham Hussaini <aheteshamhussaini0652@gmail.com>
2023-04-19 23:48:23 +0530

    hi

--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -4,11 +4,15 @@ class StyledText extends StatelessWidget {
   const StyledText(this.text, {super.key});
 
   final String text;
+
   @override
   Widget build(context) {
     return Text(
       text,
-      style: const TextStyle(fontSize: 28, color: Colors.white),
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 28,
+      ),
     );
   }
 }
